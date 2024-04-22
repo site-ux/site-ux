e9221cc08f33fc43cc65496f2b17da36173f3e07
Ellen Guimarães <104315381+EllenGui@users.noreply.github.com>
2024-04-22 10:59:12 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Oculta todos os conteúdos ao carregar a página
+  const allContents = document.querySelectorAll('.info-container');
+  allContents.forEach(content => {
+    content.style.display = 'none';
+  });
+});
+
+function showInfo(info) {
+
+  // Oculta todos os conteúdos
+  const allContents = document.querySelectorAll('.info-container');
+  allContents.forEach(content => {
+    content.style.display = 'none';
+  });
+
+  // Exibe o conteúdo correspondente ao item do menu clicado
+  const selectedContent = document.getElementById(info);
+  selectedContent.style.display = 'block';
+  // Espera um curto período antes de rolar a página
+  setTimeout(() => {
+    selectedContent.scrollIntoView({ behavior: 'smooth' });
+  }, 100);
+
+}
+
+// Função para atualizar a data no rodapé
+function updateFooterDate() {
+  const footerDate = document.getElementById('footerDate');
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString('pt-BR', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+  footerDate.textContent = `Data: ${formattedDate}`;
+}
+
+// Chama a função para atualizar a data quando a página for carregada
+window.addEventListener('load', updateFooterDate);
+
